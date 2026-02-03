761204a58ee6038251933af783d703e93c53e781
Vktr22 <nds.viktoria@gmail.com>
2026-02-03 01:00:09 +0100

    kattintas kezelo

--- src/components/MiniTermek.js
+++ src/components/MiniTermek.js
@@ -2,8 +2,13 @@ import React from 'react'
 import './MiniTermek.css';
 
 function MiniTermek(props) {
+
+  function kattintasKezelo(){
+    props.kattintas(props.adat);
+  }
+    
   return (
-        <div className="mini-termek"> 
+        <div className="mini-termek" onClick={kattintasKezelo}> 
             <img src={props.adat.kep} alt={props.adat.nev} /> 
         </div> 
 
